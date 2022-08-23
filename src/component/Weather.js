d36856b5b460c2217693b923a2f2afae99ad3f3c
Gerald Ezenagu <egerald344@gmail.com>
2022-08-23 20:19:08 +0100

    added html elements

---
+++ src/component/Weather.js
@@ -0,0 +1,32 @@
+import './Weather.css'
+
+const Weather = () => {
+    return ( 
+        <div className='weather'>
+            <h1 className="weather__title">Weather App.</h1>
+
+            <form>
+                <label htmlFor='city'>Enter a location for weather information</label>
+                <input type='text' name='city' />
+            </form>
+
+            <div className="weather__info">
+               <a href="https:placeholder.com"><img src="https://via.placeholder.com/400" alt='img' /></a>
+                <div>
+                    {/* icon */}
+                </div>
+
+                <div>
+                    <h5>City name</h5>
+                    <div>Weather condition</div>
+                    <div>
+                        <span>temp</span>
+                        <span>&deg;C</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default Weather;
