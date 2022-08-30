583fc2fba48b33508165921af3e86c15f01d8e4e
Gerald Ezenagu <egerald344@gmail.com>
2022-08-31 00:51:13 +0100

    working on new app

--- src/component/Weather.js
+++ src/component/Weather.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './Weather.css';
 
 const api = {
     key: "b7d22a0e223b2e67998ce6bd1a6c59f7",
@@ -6,6 +7,22 @@ const api = {
 }
 
 const Weather = () => {
+
+    const dateBuilder = (d) => {
+        let months = ["January", "February", "March", "April", "May", "June", "July",
+          "August", "September", "October", "November", "December"
+        ];
+        let days = ["Sunday", "Monday", "Tuesday", 
+            "Wednesday", "Thursday", "Friday", "Saturday"
+        ];
+
+        let day = days[d.getDay()];
+        let date = d.getDate();
+        let month = months[d.getMonth()];
+        let year = d.getFullYear();
+
+        return `${day} ${date} ${month} ${year}`
+    }
     return ( 
         <main>
             <div className="search-box">
@@ -14,6 +31,16 @@ const Weather = () => {
                   className="search-bar"
                   placeholder="Search..."
                 />
+            </div>
+            <div className="location-box">
+                <div className="location">Naija, NGR</div>
+                <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-box">
+                <div className="temp">
+                    15&deg;C
+                </div>
+                <div className="weather">Sunny</div> 
             </div>
         </main>
      );
